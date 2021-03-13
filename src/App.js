d791e08cbbfc2aa14f4a986c5656ae5a3262a5d8
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-03-12 19:32:18 -0600

    Updated App.js and Index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Project from './components/Project';
+import Wrapper from './components/Wrapper';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <div>
+       <Navbar />
+       <Header />
+       <Wrapper>
+         <Route exact path="/" component={About} />
+         <Route exact path="/about" component={About} />
+         <Route exact path="/contact" component={Contact} />
+         <Route exact path="/portfolio" component={Portfolio} />
+       </Wrapper>
+       <Footer />
+     </div>
+   </Router>
   );
 }
 
