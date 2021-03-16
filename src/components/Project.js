91a7240352945f2cb58bae3fda33ce88bc7469ad
Rahul Chaphekar <rchaphekar1@gmail.com>
2021-03-16 11:12:17 -0500

    Added remaining projects to Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import Picture from '../assets/Project1.png'
+import NightOut from '../assets/Project1.png';
+import DeedDone from '../assets/DeedDone.png';
+import EmployeeDirectory from '../assets/EmployeeDirectory.png';
+import BudgetTracker from '../assets/BudgetTracker.png';
+import PasswordGenerator from '../assets/PasswordGenerator.png';
 import '../style.css';
 
 function Project() {
@@ -7,10 +11,31 @@ function Project() {
         <div class="row no-gutters" style={{marginBottom: "20px"}}>
             <div class="col-md-6">
                 <a href="https://jwhendershott.github.io/NightOut/#" target="_blank" rel="noreferrer">
-                    <img class="img img-thumbnail" src={Picture} alt="Project One" width= "350px" height= "350px"></img>
+                    <img class="img img-thumbnail" src={NightOut} alt="Night Out" width= "350px" height= "350px"></img>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="https://deeddone.herokuapp.com/" target="_blank" rel="noreferrer">
+                    <img class="img img-thumbnail" src={DeedDone} alt="Deed Done" width= "350px" height= "350px"></img>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="https://rchaphekar1.github.io/employeedirectory/" target="_blank" rel="noreferrer">
+                    <img class="img img-thumbnail" src={EmployeeDirectory} alt="Employee Directory" width= "350px" height= "350px"></img>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="https://budgettracker-project.herokuapp.com/" target="_blank" rel="noreferrer">
+                    <img class="img img-thumbnail" src={BudgetTracker} alt="Budget Tracker" width= "350px" height= "350px"></img>
+                </a>
+            </div>
+            <div class="col-md-6">
+                <a href="https://rchaphekar1.github.io/PasswordGenerator/" target="_blank" rel="noreferrer">
+                    <img class="img img-thumbnail" src={PasswordGenerator} alt="Password Generator" width= "350px" height= "350px"></img>
                 </a>
             </div>
         </div>
+        
     )
 };
 
